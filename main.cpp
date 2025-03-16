a6051b028c7d32747889feddecf590f2b0bada12
vaesag <go.victor.a@gmail.com>
2025-03-16 12:54:19 +0500

    Создание основы SFML проекта в main

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
-// main.cpp
-
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+int main() {
+    // Создаем фиксированное окно 800x600
+    sf::RenderWindow window(sf::VideoMode(700, 550), "SlotGames", sf::Style::Titlebar | sf::Style::Close);
 
-    while (window.isOpen())
-    {
+    // Загружаем текстуру для фона
+    sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("background.png")) {
+        return -1; // Ошибка, если файл не найден
+    }
+
+    // Создаем спрайт для фона
+    sf::Sprite backgroundSprite;
+    backgroundSprite.setTexture(backgroundTexture);
+
+    // Главный цикл игры
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
+        // Отрисовка
         window.clear();
+        window.draw(backgroundSprite); // Рисуем фон
         window.display();
     }
 
